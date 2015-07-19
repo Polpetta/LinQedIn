f23474ad3e98ace1056e1151856bcbe47ff1ce28
Davide Polonio <poloniodavide@gmail.com>
2015-07-19 17:49:26 +0200

    Modifiche per test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 //#include <QApplication>
+#include "personal.h"
+#include <QDate>
+
+
+//#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    /*
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
 
     //return a.exec();
+
+    QDate nascita(1994,8,27);
+
+    Personal prova(nascita,
+                   "Davide",
+                   "Polonio",
+                   "3204961582",
+                   "poloniodavide@gmail.com",
+                   false);
+
+    prova.addHobby("Computer");
+    prova.addHobby("Giardinaggio");
+
+    prova.addInterest("SysAdmin");
+
+    std::cout<<"Fatto"<<std::endl;
+
+    prova.print();
+    */
 }
