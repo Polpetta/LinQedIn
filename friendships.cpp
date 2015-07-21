6ac0c44d82e49d9838415d946072ea975e651256
Davide Polonio <poloniodavide@gmail.com>
2015-07-21 16:51:30 +0200

    Implementazione della classe

--- friendships.cpp
+++ friendships.cpp
@@ -1,7 +1,69 @@
 #include "friendships.h"
 
 Friendships::Friendships()
-{
+{}
 
+void Friendships::add(const int & newId){
+
+    vector<int>::push_back(newId);
 }
 
+void Friendships::rm(const int & target){
+
+    vector<int>::iterator it;
+
+    for (it = vector<int>::begin(); it != vector<int>::end(); ++it){
+
+        if (*it == target){
+
+            int last = vector<int>::back();
+            vector<int>::pop_back();
+
+            *it = last;
+
+        }
+
+    }
+}
+
+Friendships::iterator::iterator() {}
+
+Friendships::iterator::iterator (const vector<int>::iterator & itr)
+    : vector<int>::iterator(itr) {}
+
+Friendships::iterator Friendships::begin(){
+
+    return vector<int>::begin();
+}
+
+Friendships::iterator Friendships::end(){
+
+    return vector<int>::end();
+}
+
+int & Friendships::operator [] (const Friendships::iterator & it) const{
+
+    return *it;
+}
+
+
+
+Friendships::const_iterator::const_iterator(){}
+
+Friendships::const_iterator::const_iterator(const vector<int>::const_iterator & itr)
+    : vector<int>::const_iterator(itr) {}
+
+Friendships::const_iterator Friendships::cbegin() const{
+
+    return vector<int>::begin();
+}
+
+Friendships::const_iterator Friendships::cend() const{
+
+    return vector<int>::end();
+}
+
+const int & Friendships::operator [] (const Friendships::const_iterator & it)const{
+
+    return *it;
+}
--- friendships.h
+++ friendships.h
@@ -1,11 +1,43 @@
 #ifndef FRIENDSHIPS_H
 #define FRIENDSHIPS_H
 
+#include <vector>
 
-class Friendships
+using std::vector;
+
+
+class Friendships : private vector<int>
 {
 public:
     Friendships();
+
+    void add (const int &);
+    void rm (const int &);
+
+    class iterator : public vector<int>::iterator{
+
+    public:
+        iterator();
+        iterator (const vector<int>::iterator &);
+    };
+
+    Friendships::iterator begin();
+    Friendships::iterator end();
+
+    int & operator[] (const Friendships::iterator &) const;
+
+    class const_iterator : public vector <int>::const_iterator{
+
+    public:
+        const_iterator();
+        const_iterator (const vector<int>::const_iterator &);
+    };
+
+    Friendships::const_iterator cbegin() const;
+    Friendships::const_iterator cend() const;
+
+    const int & operator[] (const Friendships::const_iterator &) const;
+
 };
 
 #endif // FRIENDSHIPS_H
