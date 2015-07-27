f508e18ae4c2f23da727d6ce8879ebb939b77c5a
Davide Polonio <poloniodavide@gmail.com>
2015-07-27 12:06:29 +0200

    Integrazione con la classe UserData

--- database.cpp
+++ database.cpp
@@ -5,17 +5,17 @@ Database::Database() : state(ok)
 
 Database::~Database() {}
 
-void Database::add(const SmartUtente & newUser) {
+/*void Database::add(const SmartUtente & newUser) {
 
     db.push_back(newUser);
 }
 
 void Database::remove(const SmartUtente & target) {
     //DA RIVEDERE
-    /*
+    *
      * Da rivedere in quanto bisogna controllare come si comporta la
      * RAM in queste situazioni.
-     */
+     *
 
 
     vector<SmartUtente>::iterator it;
@@ -30,36 +30,51 @@ void Database::remove(const SmartUtente & target) {
             *it = last;
         }
     }
+}*/
+
+
+UserData& Database::getDb(){
+
+    return db;
+}
+
+const UserData& Database::cgetDb() const{
+
+    return db;
 }
 
 
 bool Database::isOk()const{
+    dbState confront = ok;
 
-    if (state == dbState::ok)
+    if (state == confront)
         return true;
 
     return false;
 }
 
 bool Database::isRead_only()const{
+    dbState confront = read_only;
 
-    if (state == dbState::read_only)
+    if (state == confront)
         return true;
 
     return false;
 }
 
 bool Database::isBad_db()const{
+    dbState confront = bad_db;
 
-    if (state == dbState::bad_db)
+    if (state == confront)
         return true;
 
     return false;
 }
 
 bool Database::isGeneric_error()const{
+    dbState confront = generic_error;
 
-    if (state == dbState::generic_error)
+    if (state == confront)
         return true;
 
     return false;
--- database.h
+++ database.h
@@ -3,11 +3,12 @@
 
 #include <vector>
 #include "smartutente.h"
+#include "userdata.h"
 
 using std::vector;
 
 class Database
-{
+{    
 public:
     enum dbState{
         ok = 0, //tutto bene
@@ -21,37 +22,42 @@ public:
 
     virtual void save() =0; //il vector viene scritto su file
     virtual void load() =0; //carica tutto il db
-    virtual void add (const SmartUtente &); //aggiunge un singolo utente
-    virtual void remove (const SmartUtente &); //rimuove un utente singolo
 
-    virtual vector<SmartUtente> & search (const User &) =0;
+    UserData& getDb();
+    const UserData& cgetDb() const; //per sola lettura
+    /*
+     * Nota: UserData è definito esterno perchè così la funzione
+     * search può ritornare un UserData (che sarà un sottoinsieme
+     * del db) e quindi c'è un unica implementazione per la gestione
+     * di tutti i dati o sottoinsiemi di dati del db.
+     */
+
+    //virtual void add (const SmartUtente &); //aggiunge un singolo utente
+    //virtual void remove (const SmartUtente &); //rimuove un utente singolo
+
+    virtual UserData & search (const User &) =0;
     /*Qui forse non è cosa buona e giusta passare un intero User
      * perchè ci sarebbe un sacco di utilizzo di memoria. Forse
      * sarebbe meglio creare una gerarchia di classe "criteria" che
      * in base al tipo di utente che effettua la ricerca
-     * contiene solamente i dati necessari */
+     * contiene solamente i dati necessari.
+     * È necessario sia virtual? Si potrebbe implementare a questo
+     * livello?
+     */
 
     //stati del db
     bool isOk() const;
     bool isRead_only() const;
     bool isBad_db() const;
     bool isGeneric_error() const;
-    void setState(dbState =dbState::ok); //pone state a 0
-
-protected:
-    /*
-     * è giusto mettere protected?
-     * Potrebbe essere una buona scelta per rendere facilmente
-     * accessibile la lista alla classi derivate, ma è una buona
-     * implementazione dal punto di vista dell'information hiding?
-     * Forse bisogna fare un'altra classe al posto di usare
-     * semplicemente un vector? In caso non si usi più un vector,
-     * tutte le sottoclassi dovrebbero adattarsi a questo cambiamento
-     */
-    vector<SmartUtente> db;
+    void setState(dbState =ok); //pone state a 0
 
 private:
+
+    UserData db;
     dbState state;
+
+
 };
 
 #endif // DATABASE_H
