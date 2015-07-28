3a3265901cc225b22b5444feeb2a5ef068bd2aec
Davide Polonio <poloniodavide@gmail.com>
2015-07-28 15:58:29 +0200

    Scritta bozza su gestione del database nel formato Xml

---
+++ dbonxml.cpp
@@ -0,0 +1,48 @@
+#include "dbonxml.h"
+
+DBonXml::DBonXml(const QString & nameFile)
+    : DBonFile(nameFile),
+      read (this),
+      write (this)
+    /*
+     * Come costruttori di read e write ho messo this
+     * in quanto i loro costruttori accettano un QIODevice*,
+     * ma essendo che io derivo da QIODevice (perchè DBonIO
+     * deriva da QIODevice) allora posso inizializzarli su me
+     * stesso (ovvero sul file in cui scriverò) perchè punto a
+     * una sottoclasse della classe base, usando il polimorfismo.
+     */
+{}
+
+DBonXml::~DBonXml()
+{}
+
+
+void DBonXml::save(){
+
+    if (QIODevice::open(QIODevice::WriteOnly) != true){
+        dbState dst = read_only;
+        setState(dst);
+        return; //non posso scriverci, esco
+    }
+
+    write.setAutoFormatting( true );
+
+    write.writeStartDocument();
+
+    write.writeStartElement("Users");
+
+    /*
+     * va scritta la funzione save di user (dei suoi sottooggetti)
+     * poi qui viene invocata
+     */
+}
+
+
+void DBonXml::load(){
+
+    /*
+     * per il momento vuota aspettando l'implementazione della
+     * save()
+     */
+}
---
+++ dbonxml.h
@@ -0,0 +1,24 @@
+#ifndef DBONXML_H
+#define DBONXML_H
+
+#include <QXmlStreamReader>
+#include <QXmlStreamWriter>
+#include <dbonfile.h>
+
+class DBonXml : public DBonFile
+{
+public:
+    DBonXml(const QString & = QString());
+    virtual ~DBonXml();
+
+    //ora posso definire come scriverò e come caricherò il db
+    virtual void save();
+    virtual void load();
+
+private:
+
+    QXmlStreamReader read;
+    QXmlStreamWriter write;
+};
+
+#endif // DBONXML_H
