7eb648a075af4a5f51f5c03444161ba99c6149b4
Davide Polonio <poloniodavide@gmail.com>
2015-08-04 11:50:44 +0200

    Cambiamenti alla classe -Passaggio da std::list a QList -Aggiunt di operator ==, operator != -Aggiunt di size()

--- experiences.cpp
+++ experiences.cpp
@@ -5,27 +5,27 @@ Experiences::Experiences()
 
 void Experiences::add(const Event & newEvent){
 
-    list<Event>::push_back(newEvent);
+    QList<Event>::push_back(newEvent);
 }
 
 void Experiences::rm(const Event & target){
 
-    list<Event>::remove(target);
+    QList<Event>::removeAll(target);
 }
 
 Experiences::iterator::iterator() {}
 
-Experiences::iterator::iterator (const list<Event>::iterator & itr)
-    : list<Event>::iterator(itr) {}
+Experiences::iterator::iterator (const QList<Event>::iterator & itr)
+    : QList<Event>::iterator(itr) {}
 
 Experiences::iterator Experiences::begin(){
 
-    return list<Event>::begin();
+    return QList<Event>::begin();
 }
 
 Experiences::iterator Experiences::end(){
 
-    return list<Event>::end();
+    return QList<Event>::end();
 }
 
 Event & Experiences::operator [] (const Experiences::iterator & it) const{
@@ -37,20 +37,37 @@ Event & Experiences::operator [] (const Experiences::iterator & it) const{
 
 Experiences::const_iterator::const_iterator() {}
 
-Experiences::const_iterator::const_iterator(const list<Event>::const_iterator & itr)
-    : list <Event>::const_iterator (itr) {}
+Experiences::const_iterator::const_iterator(const QList<Event>::const_iterator & itr)
+    : QList <Event>::const_iterator (itr) {}
 
 Experiences::const_iterator Experiences::cbegin() const{
 
-    return list<Event>::begin();
+    return QList<Event>::begin();
 }
 
 Experiences::const_iterator Experiences::cend() const{
 
-    return list<Event>::end();
+    return QList<Event>::end();
 }
 
 const Event & Experiences::operator [] (const Experiences::const_iterator & it) const {
 
     return *it;
+}
+
+
+bool Experiences::operator ==(const Experiences & as)const{
+
+    return QList<Event>::operator ==(as);
+}
+
+bool Experiences::operator !=(const Experiences & as)const{
+
+    return QList<Event>::operator !=(as);
+}
+
+
+int Experiences::size()const{
+
+    return QList<Event>::size();
 }
--- experiences.h
+++ experiences.h
@@ -1,12 +1,11 @@
 #ifndef EXPERIENCES_H
 #define EXPERIENCES_H
 
-#include <list>
+#include <QList>
 #include "event.h"
 
-using std::list;
 
-class Experiences : private list <Event>
+class Experiences : private QList <Event>
 {
 public:
     Experiences();
@@ -14,11 +13,11 @@ public:
     void add (const Event &);
     void rm (const Event &);
 
-    class iterator : public list <Event>::iterator{
+    class iterator : public QList <Event>::iterator{
 
     public:
         iterator();
-        iterator (const list<Event>::iterator &);
+        iterator (const QList<Event>::iterator &);
     };
 
     Experiences::iterator begin();
@@ -27,11 +26,11 @@ public:
     Event & operator[] (const Experiences::iterator &) const;
 
 
-    class const_iterator : public list <Event>::const_iterator{
+    class const_iterator : public QList <Event>::const_iterator{
 
     public:
         const_iterator();
-        const_iterator (const list <Event>::const_iterator &);
+        const_iterator (const QList <Event>::const_iterator &);
     };
 
     Experiences::const_iterator cbegin() const;
@@ -39,6 +38,10 @@ public:
 
     const Event & operator[] (const Experiences::const_iterator &) const;
 
+    bool operator==(const Experiences &)const;
+    bool operator!=(const Experiences &)const;
+
+    int size()const;
 };
 
 #endif // EXPERIENCES_H
