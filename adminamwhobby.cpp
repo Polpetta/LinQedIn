773edab0a46f55b86537c20901cad1e68a563504
Davide Polonio <poloniodavide@gmail.com>
2015-08-08 15:28:00 +0200

    Finita implementazione della pagina del Wizard per gli Hobby

--- adminamwhobby.cpp
+++ adminamwhobby.cpp
@@ -9,7 +9,8 @@ AdminAMWHobby::AdminAMWHobby(QWidget * ptr)
     QLabel* note = new QLabel( tr( "Nota: questo passaggio è facoltativo") );
 
     QLabel* hobby = new QLabel ( tr("Hobby") );
-    QLineEdit* hobbyEdit = new QLineEdit;
+    hobbyEdit = new QLineEdit;
+    hobbyEdit->setClearButtonEnabled( true );
 
     QPushButton* insert = new QPushButton ( tr ("Aggiungi") );
 
@@ -22,14 +23,46 @@ AdminAMWHobby::AdminAMWHobby(QWidget * ptr)
     layout->addWidget(insert, 2, 1);
 
     setLayout(layout);
+
+    //const QString & Hobby = hobbyEdit->text();
+
+    connect (insert,
+             SIGNAL (clicked()),
+             this,
+             SLOT (emitInsert()));
+
+    connect (this,
+             SIGNAL (newInsert(const QString &)),
+             this,
+             SLOT (addHobby(const QString &)));
+
+
 }
 
 AdminAMWHobby::~AdminAMWHobby(){
 
-
+    delete hobbyEdit;
 }
 
-const QVector<QString> & AdminAMWHobby::cgetHobby()const{
+const QVector<QString> & AdminAMWHobby::cgetHobby(){
 
-    return hobby;
+    QVector<QString>* aux = new QVector<QString>( hobby );
+    hobby.clear();
+
+    return *aux;
+}
+
+void AdminAMWHobby::emitInsert(){
+
+    emit newInsert(hobbyEdit->text());
+}
+
+void AdminAMWHobby::addHobby(const QString & newHobby){
+
+    qDebug()<<"Arrivo di un nuovo Hobby, size prima: "<<hobby.size();
+
+    hobby.push_back(newHobby);
+    hobbyEdit->clear();
+
+    qDebug()<<"Size ora: "<<hobby.size();
 }
--- adminamwhobby.h
+++ adminamwhobby.h
@@ -7,19 +7,36 @@
 #include <QPushButton>
 #include <QGridLayout>
 #include <QVector>
+#include <QWidget>
+
+#include <QDebug>
 
 class AdminAMWHobby : public QWizardPage
 {
+    Q_OBJECT
+
+signals:
+
+    void newInsert (const QString &);
+
+public slots:
+
+    void emitInsert();
+
+    void addHobby(const QString &);
+
 public:
     AdminAMWHobby(QWidget* = nullptr);
 
     ~AdminAMWHobby();
 
-    const QVector<QString> & cgetHobby()const;
+    const QVector<QString> & cgetHobby();
 
 private:
 
     QVector<QString> hobby;
+
+    QLineEdit* hobbyEdit;
 };
 
 #endif // ADMINAMWHOBBY_H
