c34d6d0ccff4fe1f9228b5222994fbb0dbef86cc
Davide Polonio <poloniodavide@gmail.com>
2015-08-10 18:14:14 +0200

    Aggiunta nuova pagina del wizard -Contiene il modulo per l'aggiunta di eventi. Se l'evento aggiunto non è valido viene generato un messaggio di errore

---
+++ adminamwexperiences.cpp
@@ -0,0 +1,114 @@
+#include "adminamwexperiences.h"
+
+AdminAMWExperiences::AdminAMWExperiences(QWidget* parent)
+    : QWizardPage (parent)
+{
+
+
+    setTitle ( tr("Inserimento Esperienze") );
+
+    QLabel* note = new QLabel ( tr ("Nota: questo passaggio è facoltativo" ));
+
+    QLabel* begin = new QLabel (tr("Data d'inizio (dd-mm-yyyy)"));
+    QLabel* finish = new QLabel (tr("Data di fine (dd-mm-yyyy)"));
+    QLabel* desc = new QLabel ( tr ("Breve descrizione"));
+    QLabel* where = new QLabel ( tr("Luogo"));
+
+    beginEdit = new QLineEdit;
+    finishEdit = new QLineEdit;
+    descEdit = new QLineEdit;
+    whereEdit = new QLineEdit;
+
+    QPushButton* insert = new QPushButton ( tr("Aggiungi"));
+
+    QGridLayout* layout = new QGridLayout;
+
+    layout->addWidget(note, 0,0);
+
+    layout->addWidget(begin, 1,0);
+    layout->addWidget(beginEdit, 1, 1);
+
+    layout->addWidget(finish, 2,0);
+    layout->addWidget(finishEdit,2,1);
+
+    layout->addWidget(desc, 3,0);
+    layout->addWidget(descEdit, 3,1);
+
+    layout->addWidget(where, 4,0);
+    layout->addWidget(whereEdit,4,1);
+
+    layout->addWidget(insert, 5, 1);
+
+    setLayout(layout);
+
+    connect (insert,
+             SIGNAL (clicked()),
+             this,
+             SLOT (emitEvent()));
+
+    connect(this,
+            SIGNAL (newEvent(const Event &)),
+            this,
+            SLOT (addExperiences(const Event &)));
+}
+
+AdminAMWExperiences::~AdminAMWExperiences(){
+
+    delete beginEdit;
+    delete finishEdit;
+    delete descEdit;
+    delete whereEdit;
+}
+
+const QVector<Event> & AdminAMWExperiences::cgetExperiences()const{
+
+    const QVector<Event>* aux = new QVector<Event> (experiences);
+
+    return *aux;
+}
+
+void AdminAMWExperiences::clear(){
+
+    experiences.clear();
+}
+
+void AdminAMWExperiences::emitEvent(){
+
+    /*emit newEvent (Event(beginEdit->text(),
+                         mfinishEdit->text(),
+                         descEdit->text(),
+                         whereEdit->text())); */
+
+    emit newEvent (Event(beginEdit->text(),
+                         finishEdit->text(),
+                         descEdit->text(),
+                         whereEdit->text(),
+                         "dd-MM-yyyy"));
+}
+
+void AdminAMWExperiences::addExperiences(const Event & newEvent){
+
+    if (newEvent.isValid() == false){
+
+        QMessageBox info (QMessageBox::Warning,
+                          tr ( "Errore inserimento Evento"),
+                          tr ("Alcuni parametri che hai inserito non sono"
+                              " corretti."),
+                          QMessageBox::Ok);
+
+        info.exec();
+    }else{
+
+    qDebug()<<"Arrivo di un nuovo Event, size prima: "<<experiences.size();
+
+    experiences.push_back(newEvent);
+
+    qDebug()<<"Size ora: "<<experiences.size();
+
+    beginEdit->clear();
+    finishEdit->clear();
+    descEdit->clear();
+    whereEdit->clear();
+
+    }
+}
---
+++ adminamwexperiences.h
@@ -0,0 +1,49 @@
+#ifndef ADMINAMWEXPERIENCES_H
+#define ADMINAMWEXPERIENCES_H
+
+
+#include <QWizardPage>
+#include <QLabel>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QVector>
+#include <QMessageBox>
+
+#include <QDebug>
+
+#include "event.h"
+
+class AdminAMWExperiences : public QWizardPage
+{
+    Q_OBJECT
+
+signals:
+
+    void newEvent(const Event &);
+
+public slots:
+
+    void emitEvent();
+
+    void addExperiences(const Event &);
+
+public:
+    AdminAMWExperiences(QWidget* = nullptr);
+
+    ~AdminAMWExperiences();
+
+    const QVector<Event> & cgetExperiences ()const;
+    void clear();
+
+private:
+
+    QVector<Event> experiences;
+
+    QLineEdit* beginEdit;
+    QLineEdit* finishEdit;
+    QLineEdit* descEdit;
+    QLineEdit* whereEdit;
+};
+
+#endif // ADMINAMWEXPERIENCES_H
