3445768b6b12384fbe09a80a99858cd1c5c2ebbb
Davide Polonio <poloniodavide@gmail.com>
2015-08-11 18:34:14 +0200

    Prime implementazioni -Da finire il bottone "dettagli" che visualizzerebbe un QMessageBox in base al profilo da mostrare

--- tablesearch.cpp
+++ tablesearch.cpp
@@ -1,7 +1,91 @@
 #include "tablesearch.h"
 
-TableSearch::TableSearch()
+TableSearch::TableSearch(QWidget* parent)
+    : QWidget(parent)
 {
 
+    results = new QGridLayout;
+
+    setLayout(results);
 }
 
+TableSearch::~TableSearch(){
+
+    delete results;
+}
+
+void TableSearch::addRow(const QString & nName,
+                         const QString & nSurname,
+                         const QString & nDate) {
+    TableSearch::info* newInfo = new TableSearch::info (items.count()+1,
+                                                        nName,
+                                                        nSurname,
+                                                        nDate);
+
+    items.push_back(newInfo);
+
+    int row = newInfo->cgetRow();
+
+    qDebug()<<"Riga: "<<row;
+
+    results->addWidget(newInfo->getRow(), row, 0,Qt::AlignLeft);
+    results->addWidget(newInfo->getInfo(), row, 1,Qt::AlignLeft);
+    results->addWidget(newInfo->getDate(), row, 2, Qt::AlignLeft);
+    results->addWidget(newInfo->getButton(), row, 3,Qt::AlignRight);
+}
+
+
+
+//info
+
+TableSearch::info::info(const int & row,
+                        const QString & nName,
+                        const QString & nSurname,
+                        const QString & nDate)
+    //: QWidget(0)
+{
+
+
+    QString SRow(QString::number(row));
+    rowNumber = new QLabel (SRow);
+
+    information = new QLabel (nName+" "+
+                              nSurname+" ");
+
+    date = new QLabel ( tr("Data di nascita: " )+ nDate);
+
+    details = new QPushButton (tr ("Dettagli ") );
+}
+
+TableSearch::info::~info(){
+
+    delete rowNumber;
+    delete information;
+}
+
+QLabel* TableSearch::info::getInfo(){
+
+    return information;
+}
+
+QLabel* TableSearch::info::getDate(){
+
+    return date;
+}
+
+QLabel* TableSearch::info::getRow(){
+
+    return rowNumber;
+}
+
+int TableSearch::info::cgetRow()const{
+
+    qDebug()<<"Text della rowNumber: "<<rowNumber->text();
+
+    return rowNumber->text().toInt();
+}
+
+QPushButton* TableSearch::info::getButton(){
+
+    return details;
+}
--- tablesearch.h
+++ tablesearch.h
@@ -1,11 +1,59 @@
 #ifndef TABLESEARCH_H
 #define TABLESEARCH_H
 
+#include <QWidget>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QList>
+#include <QLabel>
 
-class TableSearch
+#include <QDebug>
+
+
+class TableSearch : public QWidget
 {
+    Q_OBJECT
+
 public:
-    TableSearch();
+    TableSearch(QWidget* = nullptr);
+
+    ~TableSearch();
+
+    void addRow(const QString &,
+                const QString &,
+                const QString &);
+
+
+private:
+
+    class info //: public QWidget
+    {
+
+    public:
+        info (const int &,
+              const QString &,
+              const QString &,
+              const QString &);
+
+        ~info();
+
+        QLabel* getInfo();
+        QLabel* getDate();
+        QLabel* getRow();
+        QPushButton* getButton();
+        int cgetRow()const;
+
+    private:
+
+        QLabel* rowNumber;
+        QLabel* information;
+        QLabel* date;
+        QPushButton* details;
+
+    };
+
+    QGridLayout* results;
+    QList<TableSearch::info*> items;
 };
 
 #endif // TABLESEARCH_H
