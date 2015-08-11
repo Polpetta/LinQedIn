bf1cc87212ebd0ae8baffa9385047ea8ed0a1969
Davide Polonio <poloniodavide@gmail.com>
2015-08-11 18:33:25 +0200

    Realizzata tabella per mostrare i risultati di ricerca

---
+++ tablesearch.cpp
@@ -0,0 +1,7 @@
+#include "tablesearch.h"
+
+TableSearch::TableSearch()
+{
+
+}
+
---
+++ tablesearch.h
@@ -0,0 +1,11 @@
+#ifndef TABLESEARCH_H
+#define TABLESEARCH_H
+
+
+class TableSearch
+{
+public:
+    TableSearch();
+};
+
+#endif // TABLESEARCH_H
