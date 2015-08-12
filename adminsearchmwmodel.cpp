753df2434b114a94ce6a7e41791b900d34c27e0b
Davide Polonio <poloniodavide@gmail.com>
2015-08-12 11:59:56 +0200

    Implementati buffer per tenere temporaneamente Hobby e Interests

--- adminsearchmwmodel.cpp
+++ adminsearchmwmodel.cpp
@@ -5,3 +5,28 @@ AdminSearchMWModel::AdminSearchMWModel()
 
 }
 
+const QVector<QString> & AdminSearchMWModel::cgetHobby()const{
+
+    return hobbyBuffer;
+}
+
+const QVector<QString> & AdminSearchMWModel::cgetInterests() const{
+
+    return interestsBuffer;
+}
+
+void AdminSearchMWModel::addH(const QString & nHobby){
+
+    hobbyBuffer.push_back(nHobby);
+}
+
+void AdminSearchMWModel::addI(const QString & nInterests){
+
+    interestsBuffer.push_back(nInterests);
+}
+
+void AdminSearchMWModel::wipeBuffers(){
+
+    hobbyBuffer.clear();
+    interestsBuffer.clear();
+}
--- adminsearchmwmodel.h
+++ adminsearchmwmodel.h
@@ -2,10 +2,26 @@
 #define ADMINSEARCHMWMODEL_H
 
 
+#include <QVector>
+
 class AdminSearchMWModel
 {
 public:
     AdminSearchMWModel();
+
+    const QVector<QString> & cgetHobby()const;
+    const QVector<QString> & cgetInterests()const;
+
+    void addH(const QString &);
+    void addI(const QString &);
+
+    void wipeBuffers();
+
+
+private:
+
+    QVector<QString> hobbyBuffer;
+    QVector<QString> interestsBuffer;
 };
 
 #endif // ADMINSEARCHMWMODEL_H
