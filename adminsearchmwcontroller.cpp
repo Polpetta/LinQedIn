f2bd99c2857c7651e1d441b5498fa4d73893a894
Davide Polonio <poloniodavide@gmail.com>
2015-08-12 11:55:47 +0200

    Aggiunta parte implementativa della ricerca -I dati vengono raccolti e inviati al controller del pannello amministrativo che si occupa di effettuare la ricerca nel Db. Nella raccolta dei dati viene eseguito un check di consistenza su di essi.

--- adminsearchmwcontroller.cpp
+++ adminsearchmwcontroller.cpp
@@ -17,6 +17,26 @@ AdminSearchMWController::AdminSearchMWController(AdminSearchMWModel* nModel,
              SIGNAL (requestClose( QCloseEvent * )),
              this,
              SLOT (closeView( QCloseEvent* )));
+
+    connect (view,
+             SIGNAL (newHobbyFilter(const QString &)),
+             this,
+             SLOT (newHobby(const QString &)));
+
+    connect (view,
+             SIGNAL (newInterestsFilter(const QString &)),
+             this,
+             SLOT (newInterests(const QString &)));
+
+    connect (view,
+             SIGNAL (searchConfirm (const QString &,
+                                    const QString &,
+                                    const QString &)),
+             this,
+             SLOT (execSearch (const QString &,
+                               const QString &,
+                               const QString &)));
+
     //ci andranno le varie connect
 }
 
@@ -30,4 +50,110 @@ AdminSearchMWController::~AdminSearchMWController()
 void AdminSearchMWController::showUI()const{
 
     view->show();
+}
+
+void AdminSearchMWController::newHobby(const QString & nHobby) const{
+
+    model->addH(nHobby);
+}
+
+void AdminSearchMWController::newInterests(const QString & nInterests) const{
+
+    model->addI(nInterests);
+}
+
+void AdminSearchMWController::messageError(const ParserError & err) const{
+
+    QString error;
+
+    if (err == ParserError::bad_date){
+
+        error = tr ("La data non risponde al formato dd-mm-yyyy");
+
+    }else if (err == ParserError::bad_name){
+
+        error = tr ("Il nome può contenere solamente [a-z] o [A-Z] con spazi");
+
+    }else if (err == ParserError::bad_surname){
+
+        error = tr ("Il cognome può contenere solamente [a-z] o [A-Z] con spazi");
+    }
+
+    QMessageBox info(QMessageBox::Critical,
+                     tr ("Errore nel parsing dei dati"),
+                     error,
+                     QMessageBox::Ok);
+
+    info.exec();
+
+}
+
+void AdminSearchMWController::execSearch(const QString & sName,
+                                         const QString & sSurname,
+                                         const QString & sDate) const{
+
+    QDate data (QDate::fromString(sDate, "dd-MM-yyyy"));
+
+    if (sDate.size() >0 && data.isValid() == false){
+
+        messageError(ParserError::bad_date);
+        return;
+    }
+
+    QRegularExpression nameRe;
+    QRegularExpression surnameRe;
+
+    QString patternName = "([\\w+])*";
+
+    nameRe.setPattern(patternName);
+    surnameRe.setPattern(patternName);
+
+    nameRe.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+    surnameRe.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+
+    QRegularExpressionMatch nameMatch = nameRe.match(sName);
+    QRegularExpressionMatch surnameMatch = surnameRe.match(sSurname);
+
+    if (nameMatch.hasMatch() == false){
+
+        messageError(ParserError::bad_name);
+        return;
+    }
+
+    if (surnameMatch.hasMatch() == false){
+
+        messageError(ParserError::bad_surname);
+        return;
+    }
+
+    const QVector<QString> & sHobby= model->cgetHobby();
+    const QVector<QString> & sInterests = model->cgetInterests();
+
+    model->wipeBuffers();
+
+    Personal tmpP (Bio(sName,
+                       sSurname,
+                       data),
+                   sHobby,
+                   sInterests);
+
+    Profile toSearch (tmpP);
+
+    emit querySearch(toSearch);
+
+}
+
+void AdminSearchMWController::setSearchResults(const DataMember & result) const{
+
+    //da implementare
+    //Nota: bisogna far visualizzare un avviso se non ci sono stati risultati
+    if (result.size() == 0){
+
+        QMessageBox info (QMessageBox::Information,
+                          tr ("Ricerca senza risultati" ),
+                          tr ("La ricerca non ha prodotto risultati"),
+                          QMessageBox::Ok);
+
+        info.exec();
+    }
 }
--- adminsearchmwcontroller.h
+++ adminsearchmwcontroller.h
@@ -5,8 +5,16 @@
 #include <QObject>
 #include <QCloseEvent>
 
+#include <QDate>
+#include <QRegularExpression>
+#include <QRegularExpressionMatch>
+#include <QMessageBox>
+
 #include "adminsearchmwmodel.h"
 #include "adminsearchmwview.h"
+#include "datamember.h"
+
+#include "profile.h"
 
 class AdminSearchMWController : public QObject
 {
@@ -20,15 +28,33 @@ public:
 
     void showUI()const;
 
+    void setSearchResults(const DataMember &)const;
+
 signals:
 
     void resumeAdmin()const;
+    void querySearch(const Profile &)const;
 
 public slots:
 
     void closeView( QCloseEvent* )const;
 
+    void newHobby(const QString &)const;
+    void newInterests(const QString &)const;
+
+    void execSearch(const QString &,
+                    const QString &,
+                    const QString &)const;
+
 private:
+
+    enum ParserError{
+        bad_date = 0,
+        bad_name = 1,
+        bad_surname = 2
+    };
+
+    void messageError (const ParserError &)const;
 
     AdminSearchMWModel* model;
     AdminSearchMWView* view;
