bba94356b3b1592f4889b16e66737b47ab60e360
Davide Polonio <poloniodavide@gmail.com>
2015-08-30 21:36:10 +0200

    Rimosso vecchio codice commentato

--- msearchviewbasic.cpp
+++ msearchviewbasic.cpp
@@ -4,27 +4,6 @@ MSearchViewBasic::MSearchViewBasic(QWidget* ptr)
     : MSearchView(ptr)
 {
 
-    /* Per MSearchViewBusiness sta parte
-    QLabel* hobby = new QLabel ( tr("Hobby") );
-    hobbyEdit->setClearButtonEnabled( true );
-
-    QGridLayout* layoutOpt = new QGridLayout;
-    layoutOpt->addWidget(hobby, 0, 0);
-    layoutOpt->addWidget(hobbyEdit, 0, 1);
-    layoutOpt->addWidget(addHobby, 0, 2);
-
-    QGroupBox* opt = new QGroupBox ( tr("Filtro Hobby") );
-
-    opt->setLayout(layoutOpt);
-
-    MSearchView::getLayout()->addWidget(opt);
-
-
-    connect (addHobby,
-             SIGNAL (clicked()),
-             this,
-             SLOT (addFilterH()));
-    */
 }
 
 
